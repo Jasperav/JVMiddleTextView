83ea46df44629c419321ce5758dc2fa4f75098d5
Jasper <Jasperav@hotmail.com>
2019-06-28 17:12:22 +0200

    commit

--- JVMiddleTextView.podspec
+++ JVMiddleTextView.podspec
@@ -1,21 +1,7 @@
-#
-# Be sure to run `pod lib lint JVMiddleTextView.podspec' to ensure this is a
-# valid spec before submitting.
-#
-# Any lines starting with a # are optional, but their use is encouraged
-# To learn more about a Podspec see https://guides.cocoapods.org/syntax/podspec.html
-#
-
 Pod::Spec.new do |s|
   s.name             = 'JVMiddleTextView'
-  s.version          = '0.2.9'
+  s.version          = '0.3.0'
   s.summary          = 'A short description of JVMiddleTextView.'
-
-# This description is used to generate tags and improve search results.
-#   * Think: What does it do? Why did you write it? What is the focus?
-#   * Try to keep it short, snappy and to the point.
-#   * Write the description between the DESC delimiters below.
-#   * Finally, don't worry about the indent, CocoaPods strips it!
 
   s.description      = <<-DESC
 TODO: Add long description of the pod here.
@@ -26,18 +12,13 @@ TODO: Add long description of the pod here.
   s.license          = { :type => 'MIT', :file => 'LICENSE' }
   s.author           = { 'Jasper' => 'Jasperav@hotmail.com' }
   s.source           = { :git => 'https://github.com/Jasperav/JVMiddleTextView.git', :tag => s.version.to_s }
-  # s.social_media_url = 'https://twitter.com/<TWITTER_USERNAME>'
 
-  s.ios.deployment_target = '11.0'
+
+  s.ios.deployment_target = '12.0'
 
   s.source_files = 'JVMiddleTextView/Classes/**/*'
   
-  # s.resource_bundles = {
-  #   'JVMiddleTextView' => ['JVMiddleTextView/Assets/*.png']
-  # }
-
-  # s.public_header_files = 'Pod/Classes/**/*.h'
-  # s.frameworks = 'UIKit', 'MapKit'
    s.dependency 'JVConstraintEdges'
    s.dependency 'JVView'
+   s.dependency 'JVDebugProcessorMacros'
 end
--- JVMiddleTextView/Classes/MiddleTextView.swift
+++ JVMiddleTextView/Classes/MiddleTextView.swift
@@ -5,9 +5,6 @@ import JVDebugProcessorMacros
 
 open class MiddleTextView: UIView {
     
-    /// Content type for the label
-    public static var contentType: ContentTypeJVLabel!
-    
     private static let constraintEdgesWidth: CGFloat = 5
     private static let spacingFromMiddle: CGFloat = 5
     
@@ -21,54 +18,53 @@ open class MiddleTextView: UIView {
     
     private let notQueryingText: String
     private let queryingText: String?
-    private let label: JVLabel
-    private let loadingIndicator = UIActivityIndicatorView(style: .gray)
+    private let label = UILabel() // Use text setup initalizer
+    private let loadingIndicator = UIActivityIndicatorView(style: .medium)
     
     public init(notQueryingText: String, queryingText: String? = nil) {
         self.notQueryingText = notQueryingText
         self.queryingText = queryingText
-        label = JVLabel(contentType: MiddleTextView.contentType)
         
         super.init(frame: CGRect.zero)
         
-        setupLabel()
-        setupLoadingIndicator()
-        
-        change(mode: queryingText == nil ? .notQuerying : .querying)
+        layoutLabel()
+        layoutLoadingIndicator()
     }
     
     public required init?(coder aDecoder: NSCoder) {
-        Unsupported()
+        fatalError()
     }
     
-    public func change(mode: Mode) {
+    public func change(mode: Mode, customText: String? = nil) {
         switch mode {
         case .notQuerying:
-            label.text = notQueryingText
+            label.text = customText ?? notQueryingText
             loadingIndicator.stopAnimating()
         case .querying:
-            label.text = queryingText
+            label.text = customText ?? queryingText
             loadingIndicator.startAnimating()
         }
     }
-
+    
 }
 
-extension MiddleTextView: ModelCreator {
-    private func setupLabel() {
+extension MiddleTextView: ViewLayout {
+    private func layoutLabel() {
         let edges = ConstraintEdges(width: MiddleTextView.constraintEdgesWidth, setHeightToNil: true)
         
-        label.fill(toSuperview: self, edges: edges)
+        label.layout(in: self, edges: edges)
         
         label.topAnchor.constraint(equalTo: centerYAnchor, constant: MiddleTextView.spacingFromMiddle).isActive = true
         label.textAlignment = .center
+        label.numberOfLines = 0
+        label.font = .preferredFont(forTextStyle: .headline)
     }
     
-    private func setupLoadingIndicator() {
+    private func layoutLoadingIndicator() {
         loadingIndicator.addAsSubview(to: self)
         
-        loadingIndicator.spacing(from: .bottom, to: .top, view: label, constant: MiddleTextView.spacingFromMiddle)
+        loadingIndicator.layoutAnchor(from: .bottom, to: label.topAnchor, constant: -MiddleTextView.spacingFromMiddle)
         
-        loadingIndicator.equalToCenterX = self
+        loadingIndicator.layoutEqual(centerX: self)
     }
 }
